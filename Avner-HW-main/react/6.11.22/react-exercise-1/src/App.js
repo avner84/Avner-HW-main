6e81cab172a712e092f01c9c2d3bdec806210115
avner84 <avner84@gmail.com>
2022-11-08 18:29:18 +0200

    react - homWork - ver2

--- Avner-HW-main/react/6.11.22/react-exercise-1/src/App.js
+++ Avner-HW-main/react/6.11.22/react-exercise-1/src/App.js
@@ -1,48 +1,151 @@
-function Component() {
+import { useState } from "react"
 
-    let flag = true;
+function Component() {
+   
+
+   // function checksIfBoardIsFull() {
+   //    const allSquare = document.querySelectorAll(".square");
+
+   //    let avoid = 0;
+
+   //    allSquare.forEach(element => {
+
+   //        if (element.textContent === "") {
+   //            avoid++;
+   //        }
+   //    });
+
+   //    if (avoid===0){
+   //        setTimeout(function() {alert ("game over")}, 200);
+
+   //    }
+
+
+   // }
+   
+   const [value1, setValue1] = useState("")
+   const [value2, setValue2] = useState("")
+   const [value3, setValue3] = useState("")
+   const [value4, setValue4] = useState("")
+   const [value5, setValue5] = useState("")
+   const [value6, setValue6] = useState("")
+   const [value7, setValue7] = useState("")
+   const [value8, setValue8] = useState("")
+   const [value9, setValue9] = useState("")
+
+   const [flag, changeFlag] = useState(true)
+   const [emptySquares, setEmptySquaresׂׂ] = useState(9)
+
+   
+
 
    function setXorO(event) {
 
-      event.target.textContent = flag ? "X" : "O";
-      flag = !flag;
-      checksIfBoardIsFull();
+      const idSquare = event.target.id;
+
+      DoesSquareClick(idSquare)
+    
+      switch (idSquare) {
+         case "div1":
+            setValue1(flag === true ? "X" : "O");
+            changeFlag(!flag)
+            break;
+         case "div2":
+            setValue2(flag === true ? "X" : "O");
+            changeFlag(!flag)
+            break;
+         case "div3":
+            setValue3(flag === true ? "X" : "O");
+            changeFlag(!flag)
+            break;
+         case "div4":
+            setValue4(flag === true ? "X" : "O");
+            changeFlag(!flag)
+            break;
+         case "div5":
+            setValue5(flag === true ? "X" : "O");
+            changeFlag(!flag)
+            break;
+         case "div6":
+            setValue6(flag === true ? "X" : "O");
+            changeFlag(!flag)
+            break;
+         case "div7":
+            setValue7(flag === true ? "X" : "O");
+            changeFlag(!flag)
+            break;
+         case "div8":
+            setValue8(flag === true ? "X" : "O");
+            changeFlag(!flag)
+            break;
+         case "div9":
+            setValue9(flag === true ? "X" : "O");
+            changeFlag(!flag)
+            break;
+
+         default:
+            break;
+      }
+
+      // checksIfBoardIsFull()
+      
+   }
+
+
+   function DoesSquareClick(idSquare){
+      
+      const square = document.getElementById(idSquare);
+      
+      if (square.textContent===""){
+         setEmptySquaresׂׂ(emptySquares-1)
+
+      }
+      console.log(emptySquares);
+      if (emptySquares===1){
+          setTimeout(function() {alert ("game over")}, 200);
+
+      }
 
    }
 
-   
-function checksIfBoardIsFull() {
-   const allSquare = document.querySelectorAll(".square");
 
-   let avoid = 0;
+   // function checksIfBoardIsFull() {
+   //    const allSquare = document.querySelectorAll(".square");
 
-   allSquare.forEach(element => {
+      
+      
 
-       if (element.textContent === "") {
-           avoid++;
-       }
-   });
-   
-   if (avoid===0){
-       setTimeout(function() {alert ("game over")}, 200);
-       
-   }
-   
+   //    allSquare.forEach(element => {
+         
 
-}
+   //        if (element.textContent!== "") {
+   //          setEmptySquaresׂׂ(emptySquares-1)
+   //        }
+         
+   //    });
+   //    console.log('emptySquares :', emptySquares);
+
+   //    if (emptySquares===0){
+   //        setTimeout(function() {alert ("game over")}, 200);
+
+   //    }
+      
+   // }
+
+
 
    return (
 
       <div className="board">
-         <div className="square" onClick={setXorO}></div>
-         <div className="square" onClick={setXorO}></div>
-         <div className="square" onClick={setXorO}></div>
-         <div className="square" onClick={setXorO}></div>
-         <div className="square" onClick={setXorO}></div>
-         <div className="square" onClick={setXorO}></div>
-         <div className="square" onClick={setXorO}></div>
-         <div className="square" onClick={setXorO}></div>
-         <div className="square" onClick={setXorO}></div>
+         <div className="square" id="div1" onClick={setXorO}>{value1}</div>
+         <div className="square" id="div2" onClick={setXorO}>{value2}</div>
+         <div className="square" id="div3" onClick={setXorO}>{value3}</div>
+         <div className="square" id="div4" onClick={setXorO}>{value4}</div>
+         <div className="square" id="div5" onClick={setXorO}>{value5}</div>
+         <div className="square" id="div6" onClick={setXorO}>{value6}</div>
+         <div className="square" id="div7" onClick={setXorO}>{value7}</div>
+         <div className="square" id="div8" onClick={setXorO}>{value8}</div>
+         <div className="square" id="div9" onClick={setXorO}>{value9}</div>
       </div>
    )
 }
